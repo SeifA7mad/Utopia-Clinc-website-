fbf7237c7f41500892f3582c36dae0f232c0689e
Mayar179151 <84213095+Mayar179151@users.noreply.github.com>
2021-05-15 04:44:23 +0200

    m

--- Utopia Clinic (website)/includes/script.js
+++ Utopia Clinic (website)/includes/script.js
@@ -3,18 +3,18 @@ let navToggle = () => {
         width: "toggle"
     });
 }
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
+function read() {
+    var i=0;
+    if(!i){
+        document.getElementById("more").style.display="inline";
+        document.getElementById("dots").style.display="none";
+        document.getElementById("read").innerHTML="Read less";
+        i=1;
     }
-  }
+    else{
+        document.getElementById("more").style.display="none";
+        document.getElementById("dots").style.display="inline";
+        document.getElementById("read").innerHTML="Read more";
+        i=0;
+    }
+}
